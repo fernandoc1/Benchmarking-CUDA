4127b3cad7704584fa236421f35f8bfbac45fe00
Fernando <fernando@fernando-MacBook.(none)>
2011-03-13 22:38:51 -0300

    updates

--- tmp/profReader.py
+++ tmp/profReader.py
@@ -7,10 +7,13 @@ class profReader:
 	
 	_database=None
 	_profFolderPath=None
+	_regularExpression=""
+	_executionCounteir=0
 
-	def __init__(self, profFolderPath):
+	def __init__(self, profFolderPath, regularExpression):
 		self._database=Database('results.db')
 		self._profFolderPath=profFolderPath
+		self._regularExpression=regularExpression
 
 	def _getProfFilesPath(self):
 		files=dircache.listdir(self._profFolderPath)
@@ -21,14 +24,28 @@ class profReader:
 				profFiles.append(entry)
 		return profFiles
 	
-	def _insertDataInDatabase(
+	def incrementExecutionCounter(self):
+		self._executionCounter+=1
 
-	#regex examples: "(numA numB numC)"
-	#		"(numA numB)*"
+	def _getKernelName(profFileName):
+		token=profFileName.split('.')
+		return token[1]
 
-	def readProfs(self, regex):
+	def _insertDataInDatabase(self):
+		tokens=self._regularExpression.split(' ')
+		for entry in tokens:
+			#TODO
+			print entry
+
+	#regex examples: "numA numB numC"
+
+	def readProfs(self):
 		profs=self._getProfFilesPath()
 		for entry in profs:
-			x.read
+			self._insertDataInDatabase()
+
+profr=profReader('profs', "a b c")
+profr.readProfs()
+
 
 
